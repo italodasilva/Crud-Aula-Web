b40a7004c1c444183aed8336fee38d6a42af4d32
Italo Augusto <103067240+italodasilva@users.noreply.github.com>
2022-10-11 11:14:43 -0300

    Add files via upload

---
+++ ListaWeb2Tri/ListaWeb2Tri/Views/paginas.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html', {})
+
+def home1(request):
+    return render(request, 'home1.html', {})
+
+def home2(request):
+    return render(request, 'home2.html', {})
+
+def home3(request):
+    return render(request, 'home3.html', {})
+
+def home4(request):
+    return render(request, 'home4.html', {})
